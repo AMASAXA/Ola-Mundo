dace0d508a14756b7d7c44878bd64c2a9d333032
andré matheus <amatheus.seixas@gmail.com>
2022-02-01 16:14:42 -0300

    atualizar

---
+++ SITE EXEMPLO/exemplo.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+    <title>pagina de teste</title>
+    <link href="index.html" rel="stylesheet" />
+
+</head>
+   <body>
+<header>
+          <h1>titulo da pagina</h1>
+           <p>substitulo</p>
+    </header>
+<nav>
+ <a href="">link 1</a>
+ <a href="">link 2</a>
+ <a href="">link 3</a>
+ <a href="">link 4</a>
+</nav>
+<footer>
+    rodapé da pagina
+</footer>
+
+
+
+</body>
+
+</html>
